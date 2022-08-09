4f89aac1bb3c921fa7e1b0fa80e3054419480ef0
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-09 20:30:07 +0300

    gregslist update

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({
+  listing: { id, image, description, location },
+  onRemoveListing,
+}) {
+  const [isFav, setIsFav] = useState(false);
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:6001/listings/${id}`, { method: "DELETE" });
+    onRemoveListing(id);
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isFav ? (
+          <button
+            onClick={() => setIsFav(false)}
+            className="emoji-button favorite active"
+          >
+            ★
+          </button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button
+            onClick={() => setIsFav(true)}
+            className="emoji-button favorite"
+          >
+            ☆
+          </button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button onClick={handleDeleteClick} className="emoji-button delete">
+          🗑
+        </button>
       </div>
     </li>
   );
